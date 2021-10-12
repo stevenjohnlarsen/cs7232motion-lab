34b43c0d58f62a36af0f6faf69ef20eb950c82bd
Steven Larsen <stevenlarsen@Stevens-Mini.attlocal.net>
2021-10-11 21:08:16 -0500

    I think this finished the graphs

--- MotionLab/MotionLab/ViewController.swift
+++ MotionLab/MotionLab/ViewController.swift
@@ -7,9 +7,13 @@
 
 import UIKit
 import CoreMotion
+import Charts
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ChartViewDelegate{
     
+    var pieChartToday = PieChartView()
+    var pieChartYesterDay = PieChartView()
+
     // MARK: Setup
     override func viewDidLoad() {
 	        super.viewDidLoad()
@@ -25,13 +29,55 @@ class ViewController: UIViewController {
         
         //DELETE
         self.todayDaySteps = 50
+        self.yesterDayDaySteps = 210
         self.stepGoal = 200
         //END DELETE
         
         //set up the UI elements
-        self.stepGoalLabel.text = String(self.stepGoal)
-        self.updateProgressToday()
-        self.updateProgressYesterday()
+        pieChartToday.delegate = self
+        pieChartYesterDay.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        //calculate values of the pie chart
+        var stepsDisplayedToday = 0
+        var stepsLeftToday = self.stepGoal
+        var stepsDisplayedYesterday = 0
+        var stepsLeftYesterday = self.stepGoal
+        
+        if let todaySteps = self.todayDaySteps{
+            if todaySteps > self.stepGoal {
+                stepsDisplayedToday = stepGoal
+                stepsLeftToday = 0
+            }
+            else{
+                stepsDisplayedToday = todaySteps
+                stepsLeftToday = stepGoal - todaySteps
+            }
+        }
+        if let yesterdaySteps = self.yesterDayDaySteps{
+            if yesterdaySteps > self.stepGoal {
+                stepsDisplayedYesterday = stepGoal
+                stepsLeftYesterday = 0
+            }
+            else{
+                stepsDisplayedYesterday = yesterdaySteps
+                stepsLeftYesterday = stepGoal - yesterdaySteps
+            }
+        }
+        //Setup the Today graph
+        let frameToday = CGRect(x:0,
+                     y: self.view.frame.size.height / 3.0,
+                     width: self.view.frame.size.width / 2.0,
+                     height: self.view.frame.size.height / 2.0)
+        
+        let frameYesterday = CGRect(x:self.view.frame.size.width / 2.0,
+                     y: self.view.frame.size.height / 3.0,
+                     width: self.view.frame.size.width / 2.0,
+                     height: self.view.frame.size.height / 2.0)
+        
+        self.renderChart(chart:self.pieChartToday, frame:frameToday, stepsDisplayed: stepsDisplayedToday, stepsLeft: stepsLeftToday, title: "Today")
+        self.renderChart(chart: self.pieChartYesterDay, frame: frameYesterday, stepsDisplayed: stepsDisplayedYesterday, stepsLeft: stepsLeftYesterday, title: "Yesterday")
     }
     //MARK: Properties
     private var todayDaySteps:Int? = nil
@@ -48,9 +94,6 @@ class ViewController: UIViewController {
     
     //MARK: Outlets
     @IBOutlet weak var activityImage: UIImageView!
-    @IBOutlet weak var stepProgress: UIProgressView!
-    @IBOutlet weak var stepGoalLabel: UILabel!
-    @IBOutlet weak var stepGoalYesterday: UIProgressView!
     
     //MARK: Private Functions
     private func HandleTodaySteps(pedData:CMPedometerData?, error:Error?){
@@ -100,17 +143,28 @@ class ViewController: UIViewController {
             }
         }
     }
-    
-    private func updateProgressToday(){
-        if let steps = self.todayDaySteps{
-            let value =  Float(steps) / Float(self.stepGoal)
-            self.stepProgress.setProgress(value, animated: false)
-        }
-    }
-    private func updateProgressYesterday(){
-        if let steps = self.yesterDayDaySteps{
-            let value =  Float(steps) / Float(self.stepGoal)
-            self.stepGoalYesterday.setProgress(value, animated: false)
-        }
+    private func renderChart(chart: PieChartView, frame: CGRect, stepsDisplayed: Int, stepsLeft: Int, title:String){
+        
+        var label = UILabel()
+        label.text = title
+        
+        chart.frame = frame
+        view.addSubview(chart)
+        var data = PieChartDataSet()
+        data = PieChartDataSet(entries: [
+            ChartDataEntry(x: 1, y: Double(stepsDisplayed)),
+            ChartDataEntry(x: 2, y: Double(stepsLeft))
+        ])
+        data.colors = [UIColor.init(red: 78/255.0, green: 120/255.0, blue: 85/255.0, alpha: 1), .red]
+        chart.data = PieChartData(dataSet:data)
+        chart.legend.enabled = false
+        
+        label.frame = CGRect(x:frame.minX + (frame.width / 3),
+                             y:frame.minY + (frame.height / 6),
+                             width: CGFloat(title.count) * 10,
+                             height:20)
+        
+        self.view.addSubview(label)
+        
     }
 }
